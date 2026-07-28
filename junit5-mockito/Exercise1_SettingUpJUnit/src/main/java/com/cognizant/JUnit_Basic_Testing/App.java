d1777a84fd1233052494d3939ffafdf86c1468e8
Satvik <satvikg2005@gmail.com>
2026-07-28 20:11:20 +0530

    Added JUnit 5 Exercise 1 - Setting Up JUnit

---
+++ junit5-mockito/Exercise1_SettingUpJUnit/src/main/java/com/cognizant/JUnit_Basic_Testing/App.java
@@ -0,0 +1,10 @@
+package com.cognizant.JUnit_Basic_Testing;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ junit5-mockito/Exercise1_SettingUpJUnit/src/test/java/com/cognizant/JUnit_Basic_Testing/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.cognizant.JUnit_Basic_Testing;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void testSetup() {
+        System.out.println("JUnit 5 setup successful!");
+    }
+}
