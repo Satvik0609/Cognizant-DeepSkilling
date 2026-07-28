852cc895891016bcf36c3f5d3818dd71601bad3c
Satvik <satvikg2005@gmail.com>
2026-07-28 20:28:50 +0530

    Added JUnit 5 Exercise 1 and Exercise 3

---
+++ junit5-mockito/Exercise3_Assertions/src/main/java/com/cognizant/Exercise3_Assertions/App.java
@@ -0,0 +1,10 @@
+package com.cognizant.Exercise3_Assertions;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ junit5-mockito/Exercise3_Assertions/src/test/java/com/cognizant/Exercise3_Assertions/AssertionsTest.java
@@ -0,0 +1,26 @@
+package com.cognizant.Exercise3_Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AssertionsTest {
+
+    @Test
+    void testAssertions() {
+
+        // Assert equals
+        assertEquals(5, 2 + 3);
+
+        // Assert true
+        assertTrue(5 > 3);
+
+        // Assert false
+        assertFalse(5 < 3);
+
+        // Assert null
+        assertNull(null);
+
+        // Assert not null
+        assertNotNull(new Object());
+    }
+}
