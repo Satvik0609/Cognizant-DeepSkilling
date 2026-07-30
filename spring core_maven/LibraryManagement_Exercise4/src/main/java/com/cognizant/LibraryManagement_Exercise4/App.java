a04d24b2fd5fcb1e325249812097c209c231daca
Satvik <satvikg2005@gmail.com>
2026-07-30 23:25:07 +0530

    Completed Exercise 4 - Created Maven project with Spring dependencies

---
+++ spring core_maven/LibraryManagement_Exercise4/src/main/java/com/cognizant/LibraryManagement_Exercise4/App.java
@@ -0,0 +1,13 @@
+package com.cognizant.LibraryManagement_Exercise4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
