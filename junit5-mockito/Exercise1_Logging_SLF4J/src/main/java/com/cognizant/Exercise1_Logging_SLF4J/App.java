598c7096fca5446a62aa399a8cee863a555374c6
Satvik <satvikg2005@gmail.com>
2026-07-30 21:27:52 +0530

    Added SLF4J Logging Exercise 1

---
+++ junit5-mockito/Exercise1_Logging_SLF4J/src/main/java/com/cognizant/Exercise1_Logging_SLF4J/App.java
@@ -0,0 +1,13 @@
+package com.cognizant.Exercise1_Logging_SLF4J;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
